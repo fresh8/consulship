0a9c40bf9e721dc423594d207d4c95b03ad9c116
Lee Archer <lee@connected-ventures.com>
2017-03-10 14:55:24 +0000

    Add consul env config parsing

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/connectedventures/gonfigurator"
+	"github.com/hashicorp/consul/api"
+)
+
+type ConsulConfig struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+var (
+	consulByEnv = make(map[string]*api.Client)
+)
+
+func main() {
+	var consulEnvConfig []ConsulConfig
+	consulAddr := os.Getenv("CONSUL_ADDR")
+
+	err := gonfigurator.Parse("/etc/consulship/consul-env.yaml", &consulEnvConfig)
+	if err != nil {
+		log.Fatal("Cannot read consul-env.yaml config")
+	}
+
+	consulEnvConfig = append(consulEnvConfig, ConsulConfig{
+		Name:    "local",
+		Address: consulAddr,
+	})
+
+	// Create consul clients from consul env config
+	for _, consulEnv := range consulEnvConfig {
+		config := api.DefaultConfig()
+		config.Address = consulEnv.Address
+		consulByEnv[consulEnv.Name], err = api.NewClient(config)
+		if err != nil {
+			log.Fatalf("Cannot create consul client for env %s: %s", consulEnv.Name, err.Error())
+		}
+	}
+}
